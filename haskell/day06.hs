6b3f9a04d376e447c8e620a33a3ccd22461d4c92
Ethan Tan <ethantan0007@gmail.com>
2020-12-10 22:01:05 -0600

    foldl1' -> foldr1

--- haskell/day06.hs
+++ haskell/day06.hs
@@ -1,7 +1,6 @@
 import Advent
 
 import Data.Char
-import Data.List
 import qualified Data.Set as Set
 
 p1 :: FilePath -> IO ()
@@ -12,4 +11,4 @@ p1 = interactFile $ show . sum . map go . wordsBy (== "\r") . lines
 p2 :: FilePath -> IO ()
 p2 = interactFile $ show . sum . map go . wordsBy (== "\r") . lines
   where
-    go = Set.size . foldl1' Set.intersection . map (Set.fromList . filter isAsciiLower)
+    go = Set.size . foldr1 Set.intersection . map (Set.fromList . filter isAsciiLower)
